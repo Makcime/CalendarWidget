471e49ff0b04ebdbcd6b78ab3fab62f513504ab7
Makcime <marlier.maxime@gmail.com>
2015-09-28 11:25:57 +0200

    [add] cal.url management [enh] decode utf8 [enh] update header file

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <?php
 /**
- * This example demonstrates how the Ics-Parser should be used.
+ * This example is based on the Ics-Parser- Example.
  *
  * PHP Version 5
  *
- * @category Example
- * @package  Ics-parser
- * @author   Martin Thoma <info@martin-thoma.de>
- * @license  http://www.opensource.org/licenses/mit-license.php  MIT License
- * @version  SVN: <svn_id>
- * @link     http://code.google.com/p/ics-parser/
- * @example  $ical = new ical('MyCal.ics');
- *           print_r( $ical->get_event_array() );
+ * @author   Marlier Maxime <marlier.maxime@gmail.com>
+ *
  */
+
 require 'ics-parser/class.iCalReader.php';
 
-$ical   = new ICal('sample.ics');
+// if file containing the url does not exist, use the sample ics file
+if (file_exists('cal.url')) {
+    $url = file_get_contents('cal.url');
+}else{
+    $url = 'sample.ics' ;
+    echo "<br />\n";
+    echo "This is a demo from a sample file, please add a cal.url file with the link to your calendar" ;
+    echo "<br />\n";
+    echo "<br />\n";
+}
+
+$ical   = new ICal($url);
+
 $events = $ical->events();
 
 $date = $events[0]['DTSTART'];
@@ -37,20 +44,20 @@ echo "<br />\n";
 echo '<hr/><hr/>';
 
 foreach ($events as $event) {
-    echo 'SUMMARY: ' . @$event['SUMMARY'] . "<br />\n";
-    echo 'DTSTART: ' . $event['DTSTART'] . ' - UNIX-Time: ' . $ical->iCalDateToUnixTimestamp($event['DTSTART']) . "<br />\n";
-    echo 'DTEND: ' . $event['DTEND'] . "<br />\n";
-    echo 'DTSTAMP: ' . $event['DTSTAMP'] . "<br />\n";
-    echo 'UID: ' . @$event['UID'] . "<br />\n";
-    echo 'CREATED: ' . @$event['CREATED'] . "<br />\n";
-    echo 'LAST-MODIFIED: ' . @$event['LAST-MODIFIED'] . "<br />\n";
-    echo 'DESCRIPTION: ' . @$event['DESCRIPTION'] . "<br />\n";
-    echo 'LOCATION: ' . @$event['LOCATION'] . "<br />\n";
-    echo 'SEQUENCE: ' . @$event['SEQUENCE'] . "<br />\n";
-    echo 'STATUS: ' . @$event['STATUS'] . "<br />\n";
-    echo 'TRANSP: ' . @$event['TRANSP'] . "<br />\n";
-    echo 'ORGANIZER: ' . @$event['ORGANIZER'] . "<br />\n";
-    echo 'ATTENDEE(S): ' . @$event['ATTENDEE'] . "<br />\n";
-    echo '<hr/>';
+    echo utf8_decode('SUMMARY: ' . @$event['SUMMARY'] . "<br />\n");
+    echo utf8_decode('DTSTART: ' . $event['DTSTART'] . ' - UNIX-Time: ' . $ical->iCalDateToUnixTimestamp($event['DTSTART']) . "<br />\n");
+    echo utf8_decode('DTEND: ' . $event['DTEND'] . "<br />\n");
+    echo utf8_decode('DTSTAMP: ' . $event['DTSTAMP'] . "<br />\n");
+    echo utf8_decode('UID: ' . @$event['UID'] . "<br />\n");
+    echo utf8_decode('CREATED: ' . @$event['CREATED'] . "<br />\n");
+    echo utf8_decode('LAST-MODIFIED: ' . @$event['LAST-MODIFIED'] . "<br />\n");
+    echo utf8_decode('DESCRIPTION: ' . @$event['DESCRIPTION'] . "<br />\n");
+    echo utf8_decode('LOCATION: ' . @$event['LOCATION'] . "<br />\n");
+    echo utf8_decode('SEQUENCE: ' . @$event['SEQUENCE'] . "<br />\n");
+    echo utf8_decode('STATUS: ' . @$event['STATUS'] . "<br />\n");
+    echo utf8_decode('TRANSP: ' . @$event['TRANSP'] . "<br />\n");
+    echo utf8_decode('ORGANIZER: ' . @$event['ORGANIZER'] . "<br />\n");
+    echo utf8_decode('ATTENDEE(S): ' . @$event['ATTENDEE'] . "<br />\n");
+    echo utf8_decode('<hr/>');
 }
 ?>

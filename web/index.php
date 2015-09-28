e876cb8ba9dff8ecbbdf702742d3de55636b6bf4
Your Name <marlier.maxime@gmail.com>
2015-09-28 15:35:34 +0200

    [fix] utf8

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-type: text/html; charset=iso-8859-1');
+header('Content-type: text/html; charset=UTF-8'); 
 ?>
 <?php
 /**
@@ -50,23 +50,23 @@ foreach ($events as $event) {
     $img = @$event['DESCRIPTION'];
     if (filter_var($img, FILTER_VALIDATE_URL)){
 
-        echo ('SUMMARY: ' . @$event['SUMMARY'] . "<br />\n");
-        echo ('DTSTART: ' . $event['DTSTART'] . ' - UNIX-Time: ' . $ical->iCalDateToUnixTimestamp($event['DTSTART']) . "<br />\n");
-        echo ('DTEND: ' . $event['DTEND'] . "<br />\n");
-        echo ('DTSTAMP: ' . $event['DTSTAMP'] . "<br />\n");
-        // echo ('UID: ' . @$event['UID'] . "<br />\n");
-        // echo ('CREATED: ' . @$event['CREATED'] . "<br />\n");
-        // echo ('LAST-MODIFIED: ' . @$event['LAST-MODIFIED'] . "<br />\n");
-        echo ('DESCRIPTION: ' . $img . "<br />\n");
+        echo utf8_decode('SUMMARY: ' . @$event['SUMMARY'] . "<br />\n");
+        echo utf8_decode('DTSTART: ' . $event['DTSTART'] . ' - UNIX-Time: ' . $ical->iCalDateToUnixTimestamp($event['DTSTART']) . "<br />\n");
+        echo utf8_decode('DTEND: ' . $event['DTEND'] . "<br />\n");
+        echo utf8_decode('DTSTAMP: ' . $event['DTSTAMP'] . "<br />\n");
+        // echo utf8_decode('UID: ' . @$event['UID'] . "<br />\n");
+        // echo utf8_decode('CREATED: ' . @$event['CREATED'] . "<br />\n");
+        // echo utf8_decode('LAST-MODIFIED: ' . @$event['LAST-MODIFIED'] . "<br />\n");
+        echo utf8_decode('DESCRIPTION: ' . $img . "<br />\n");
         echo "<img src=", $img, " /><br />\n";
 
-        echo ('LOCATION: ' . @$event['LOCATION'] . "<br />\n");
-        // echo ('SEQUENCE: ' . @$event['SEQUENCE'] . "<br />\n");
-        // echo ('STATUS: ' . @$event['STATUS'] . "<br />\n");
-        // echo ('TRANSP: ' . @$event['TRANSP'] . "<br />\n");
-        // echo ('ORGANIZER: ' . @$event['ORGANIZER'] . "<br />\n");
-        // echo ('ATTENDEE(S): ' . @$event['ATTENDEE'] . "<br />\n");
-        echo ('<hr/>');
+        echo utf8_decode('LOCATION: ' . @$event['LOCATION'] . "<br />\n");
+        // echo utf8_decode('SEQUENCE: ' . @$event['SEQUENCE'] . "<br />\n");
+        // echo utf8_decode('STATUS: ' . @$event['STATUS'] . "<br />\n");
+        // echo utf8_decode('TRANSP: ' . @$event['TRANSP'] . "<br />\n");
+        // echo utf8_decode('ORGANIZER: ' . @$event['ORGANIZER'] . "<br />\n");
+        // echo utf8_decode('ATTENDEE(S): ' . @$event['ATTENDEE'] . "<br />\n");
+        echo utf8_decode('<hr/>');
     }
 }
 ?>
